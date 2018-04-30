6306fdd4b33a3adc507b4db3cb3d60997bd746cf
Administrator <Administrator@D2>
2018-04-30 17:05:37 +0900

    updated 04_array.html in Ex_0427 and added Ex_0430

---
+++ Ex_0430/WebContent/js/jqueryscript.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+$(function(){
+		$(`#id`).css(`border`, `1px solid #00f`);
+		$(`#pwd`).css(`border`, `1px solid #f00`);
+		
+		$(`.row`).css(`border`, `1px dotted #0f0`)
+				.css(`background`, `#aaf`)
+				.css(`color`, `#fff`);
+		$(`table`).css(`border`, `2px solid #00f`)
+				.css(`border-collapse`, `collapse`);
+		$(`input[type=submit]`).css(`border`, `1px solid #000`);
+	});
---
+++ Ex_0430/WebContent/js/test.js
@@ -0,0 +1,11 @@
+/**
+ * JS script area
+ */
+
+function my_sum(n) {
+	let sum = 0;
+	for (var i = 0; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
